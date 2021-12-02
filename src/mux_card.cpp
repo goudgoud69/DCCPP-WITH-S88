ccd56c86ab620757aca53a7db6241782ed81fc0e
David G <david.goudard@chepamoi.eu>
2021-12-02 22:59:43 +0100

    factorise noms vars

--- src/mux_card.cpp
+++ src/mux_card.cpp
@@ -13,6 +13,8 @@ CD4051B
 #include "Config.h"
 #include "mux_card.h"
 
+#define DEBUG_MODE
+
 extern Mux_Card mux_card[];
 
 const int mux_selectPins[] = {MUX_S0_PIN, MUX_S1_PIN, MUX_S2_PIN, MUX_S3_PIN};
@@ -20,41 +22,37 @@ const int mux_selectPins[] = {MUX_S0_PIN, MUX_S1_PIN, MUX_S2_PIN, MUX_S3_PIN};
 void mux_card_init()
 {
 #if MUX_CARD_NB >= 1
+    pinMode(MUX_S0_PIN, OUTPUT);
+    pinMode(MUX_S1_PIN, OUTPUT);
+    pinMode(MUX_S2_PIN, OUTPUT);
+
+    digitalWrite(MUX_S0_PIN, LOW);
+    digitalWrite(MUX_S1_PIN, LOW);
+    digitalWrite(MUX_S2_PIN, LOW);
+   
     for (byte i = 0; i < MUX_CARD_NB; i++)
     {
-        // Active le CD4051B
-       // pinMode(mux_card[i].ENABLE_PIN, OUTPUT);
-       // digitalWrite(mux_card[i].ENABLE_PIN, LOW);
-
-        pinMode(MUX_S0_PIN, OUTPUT);
-        pinMode(MUX_S1_PIN, OUTPUT);
-        pinMode(MUX_S2_PIN, OUTPUT);
-
-        digitalWrite(MUX_S0_PIN, LOW);
-        digitalWrite(MUX_S1_PIN, LOW);
-        digitalWrite(MUX_S2_PIN, LOW);
-
-        if (mux_card[i].NB_OUT % 8 == 2)
+        if (mux_card[i].NB_CHANNEL == MUX_CARD_16_PORTS)
         {
             pinMode(MUX_S3_PIN, OUTPUT);
             digitalWrite(MUX_S3_PIN, LOW);
         }
-
-        // Désactive le CD4051B
-        pinMode(mux_card[i].ENABLE_PIN, OUTPUT);
-        digitalWrite(mux_card[i].ENABLE_PIN, HIGH);
     }
+
+    // Active le CD4051B
+    pinMode(MUX_ENABLE_PIN, OUTPUT);
+    digitalWrite(MUX_ENABLE_PIN, LOW);
 
 #endif
 }
 
 /**
  * byte num_card : num mux card
- * byte out      : n° voie
+ * byte channel      : n° voie
  * 
  * return byte level    : valeur à écrire 
  */
-byte mux_card_read(byte num_card, byte out)
+byte mux_card_read(byte num_card, byte channel)
 {
     byte val;
 
@@ -62,62 +60,27 @@ byte mux_card_read(byte num_card, byte out)
     if (num_card > MUX_CARD_NB - 1)
         return 0;
 
-    for (byte i = 0; i < MUX_CARD_NB; i++)
-    {
-        //desactive le CD4051B
-        pinMode(mux_card[i].ENABLE_PIN, OUTPUT);
-        digitalWrite(mux_card[i].ENABLE_PIN, HIGH);
-    }
+    if (channel > mux_card[num_card].NB_CHANNEL - 1)
+        return 0;
 
-
-    // Active le CD4051B
-    pinMode(mux_card[num_card].ENABLE_PIN, OUTPUT);
-    digitalWrite(mux_card[num_card].ENABLE_PIN, LOW);
-
-    //pinMode(MUX_SIG_IN_PIN, INPUT);
+    digitalWrite(MUX_S0_PIN, bitRead(channel, 0));
+    digitalWrite(MUX_S1_PIN, bitRead(channel, 1));
+    digitalWrite(MUX_S2_PIN, bitRead(channel, 2));
 
     // force les sorties
-    if (mux_card[num_card].NB_OUT > 8)
+    if (mux_card[num_card].NB_CHANNEL == MUX_CARD_16_PORTS)
     {
-        selectMuxPin16(out);
+       digitalWrite(MUX_S3_PIN, bitRead(channel, 3));
     }
-    else
-    {
-        selectMuxPin8(out);
-    }
-    val = digitalRead(MUX_SIG_IN_PIN); //analogRead(MUX_SIG_IN_PIN); utiliser digital comme cela on reçoit 0 ou 1 pour détecteur présence c'est mieux.
+    delay(2);
+
+    val = digitalRead(mux_card[num_card].SIG_IN_PIN); //analogRead(MUX_SIG_IN_PIN); utiliser digital comme cela on reçoit 0 ou 1 pour détecteur présence c'est mieux.
 #endif
+     
+    #ifdef DEBUG_MODE 
+    //Serial.print(" "); 
+    //Serial.println(val);
+    #endif
 
     return val;
-}
-
-// The selectMuxPin function sets the S0, S1, and S2 pins to select the give pin
-void selectMuxPin8(byte pin)
-{
-    if (pin > 7)
-        return; // Exit if pin is out of scope
-    selectMuxPin(pin, 3);
-}
-
-void selectMuxPin16(byte pin)
-{
-    if (pin > 15)
-        return; // Exit if pin is out of scope
-    selectMuxPin(pin, 4);
-}
-
-void selectMuxPin(byte pin, byte maxSIG)
-{
-    for (int i = 0; i < maxSIG; i++)
-    {
-        /*
-        if (DEBUG_MODE) {
-            Serial.print("Pin ");
-            Serial.print(mux_selectPins[i]);
-            Serial.print("-->");
-            Serial.println(bitRead(pin, i));
-        }
-        */
-        digitalWrite(mux_selectPins[i], bitRead(pin, i));
-    }
 }
