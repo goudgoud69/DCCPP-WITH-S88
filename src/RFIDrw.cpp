c17b7f875010010379f415d9031c150a0156ee72
David G <david.goudard@chepamoi.eu>
2021-12-07 18:11:24 +0100

    prepare

--- src/RFIDrw.cpp
+++ src/RFIDrw.cpp
@@ -6,7 +6,7 @@ COPYRIGHT (c) 2021 David Goudard
 **********************************************************************/
 
 #include "RFIDrw.h"
-#include "mcp2515.h"
+//#include "mcp2515.h"
 
 long int RFIDrw::sampleTime = 0;
 
@@ -42,17 +42,12 @@ RFID _rfid(SS,RST);
 
 void RFIDrw::init() 
 {
-    //RFID _rfid(SS,RST);
-    //RFID RFIDrw::rfid(SS,RST); // = _rfid;
-    
     #ifdef RFID_MUX_CARD
        MuxCard::init();
     #endif
 
     SPI.begin();
     _rfid.init(); 
-
-     //OldSensorStatus = SensorMuxCard::getSensorStatus();
 }
 
 boolean RFIDrw::checkTime() 
@@ -80,17 +75,13 @@ void RFIDrw::check()
     {
       #ifdef RFIDrw_DEBUG
         Serial.print("The card's number is  : ");
-        //char serialID[5];
-        //Serial.write(0x02);
         for(int i = 0; i < 4; i++)
         {
-          //serialID[i]=str[i];
           Serial.print(0x0F & (str[i] >> 4),HEX);
           Serial.print(0x0F & str[i],HEX);
         }
         Serial.println("");
       #endif
-     
 
       // lecture numero et nom du train/loco
       // Nous utilisons les pages 4 à 7 inclus (4 octets/page donc 16 octets)
@@ -108,61 +99,11 @@ void RFIDrw::check()
         dump_char_array(buffer, 16); Serial.println();
         Serial.println();
       #endif
-
-
-      send_byte_array_incl_ChkSum(str, 6);     // send card-ID (5 words) to the RS232 port in HEX, including Checksum
-
-     // RFIDrw::sendRFIDDetected(1, buffer);
     }
   
     _rfid.selectTag(str);
   }
   _rfid.halt();
-}
-/*
-void RFIDrw::sendRFIDDetected(byte can_id, byte *buffer) 
-{
-  struct can_frame canMsg;
-
-  // numero de loco
-  byte nloco = (buffer[0] - '0')*10 + (buffer[1] - '0');
-
-  canMsg.can_id = can_id; //0x036; //CAN id as 0x036
-  canMsg.can_dlc = 8; //CAN data length as 8
-  
-  canMsg.data[0] = nloco;
-  for (byte i = 1; i < 8; i++)
-  { 
-    canMsg.data[i] = buffer[i+2];
-  }
-
-
-
-  MCP2515 mcp2515(0);
-  mcp2515.sendMessage(&canMsg); //Sends the CAN message
-}
-*/
-
-/*** routine to send the UiD including STX, checksum and ETX to Serial.*/
-//void send_byte_array_incl_ChkSum(byte *buffer, byte bufferSize) 
-void send_byte_array_incl_ChkSum(unsigned char *buffer, byte bufferSize)  
-{
-  byte i=1, Checksum=0 ;
-      
-    Serial.write(0x02);                                              // STX (02h). You must send STX as a byte, only then STX is recognized as a command
-    
-    for (i = 1; i < 6; i++)                                          // Remove first and last byte, we can only manage messages with 5 bytes of data
-    {          
-      //Serial.print(0x0F & (str[i] >> 4),HEX);
-      //Serial.print(0x0F & str[i],HEX);                                                                       
-      if (i==1) Checksum  = (byte)buffer[i];
-      else      Checksum ^= (byte)buffer[i];   
-      if ((byte)buffer[i] < 0x10) Serial.print (0);                        // HEX value is always 2 bytes
-      Serial.print((byte)buffer[i],HEX);                                   // send information
-    }
-   if (Checksum < 0x10) Serial.print (0);                            // HEX value is always 2 bytes
-   Serial.println(Checksum,HEX);                                     // send checksum
-   Serial.write(0x03);                                               // ETX (03h). You must send ETX as a byte, only then ETX is recognized as a command
 }
 
  
